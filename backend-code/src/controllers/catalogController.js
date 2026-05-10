7ebc20a7eb3022ee6310dc49f6277131930fd7fd
keertana <rajendran563@gmail.com>
2026-05-10 14:36:32 +0530

    feat(catalog): await getCatalogMasters for proper asynchronous handling

--- backend-code/src/controllers/catalogController.js
+++ backend-code/src/controllers/catalogController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const { getCatalogMasters } = require("../services/catalogService");
 
 const listCatalogMasters = asyncHandler(async (req, res) => {
-  const data = getCatalogMasters();
+  const data = await getCatalogMasters();
   res.status(200).json({ success: true, data });
 });
 
